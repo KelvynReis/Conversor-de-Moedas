5b9e61c51df5ad52bb4faa64861feb22c9cd39c8
kelvyn <kelvyn2reis@gmail.com>
2021-02-19 14:49:50 -0300

    segundo commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <conversor moeda-a="USD" moeda-b="BRL"/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
+import Conversor from "./components/Conversor.vue";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Conversor
   }
 }
 </script>
---
+++ src/components/Conversor.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="conversor">
+        <h2>{{moedaA}} Para {{moedaB}}</h2>
+        <input type="text" v-model="moedaA_value" v-bind:placeholder="moedaA">
+        <input type="button" value="converter">
+        <h2>{{moedaB_value}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Converosor",
+    props: ["moedaA", "moedaB"],
+
+    data(){
+        return{
+            moedaA_value: "",
+            moedaB_value: 0
+        };
+    },
+    methods:{
+
+        converter(){
+            
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
