e60fea06d3e491d67b3f721c11660b6df3da3ceb
kelvyn <kelvyn2reis@gmail.com>
2021-02-19 15:58:43 -0300

    add api conversor

--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -2,7 +2,7 @@
     <div class="conversor">
         <h2>{{moedaA}} Para {{moedaB}}</h2>
         <input type="text" v-model="moedaA_value" v-bind:placeholder="moedaA">
-        <input type="button" value="converter">
+        <input type="button" value="converter" v-on:click="converter">
         <h2>{{moedaB_value}}</h2>
     </div>
 </template>
@@ -21,7 +21,18 @@ export default {
     methods:{
 
         converter(){
-            
+            let de_para = this.moedaA + "-" + this.moedaB;
+            let valorA = this.moedaA
+
+            let url = `https://economia.awesomeapi.com.br/all/${de_para}`;
+
+            fetch(url).then(res =>{return res.json()})
+                      .then(json=>{
+                          let cotacao = json[valorA].ask;
+                          console.log(cotacao);
+                          this.moedaB_value = (cotacao * parseFloat(this.moedaA_value)).toFixed(2);
+                      })
+
         }
     }
 
